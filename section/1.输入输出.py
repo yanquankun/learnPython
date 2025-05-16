34fd67e2659bafb9581a3a833857cc3d02767f5f
yanquankun <yanquankun@58.com>
2025-05-16 13:52:20 +0800

    add: 1.输入输出

---
+++ section/1.输入输出.py
@@ -0,0 +1,21 @@
+def answer():
+    # 输出
+    print("Hello World")
+    # 默认会换行
+    print("换行")
+    # 使用end内容进行控制
+    print("Hello World", end="\n")
+    # 不换行
+    print("Hello World", end=" ")
+    # 换行两次
+    print("Hello World", end="\n\n")
+
+    # 输入
+    # 通过变量保存输入的值，eg：
+    name = input("请输入你的名字：\n")
+    age = input("请输入你的年龄：\n")
+    # 使用f-string格式化输出
+    print(f"你的名字是{name}，你的年龄是{age}")
+
+
+answer()
