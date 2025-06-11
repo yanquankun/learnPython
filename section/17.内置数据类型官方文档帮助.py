68239cc83ad489a649eaa8369b654f24c6fd61bb
yanquankun <yanquankun@58.com>
2025-06-11 14:54:33 +0800

    add: section 17

---
+++ section/17.内置数据类型官方文档帮助.py
@@ -0,0 +1,12 @@
+"""
+@Desc: 本讲解Python内置数据类型的官方文档帮助。
+@Author: Mint.Yan
+@Date: 2025-06-162 14:51:18
+"""
+
+# 1.Python内置数据类型的官方文档地址
+# https://docs.python.org/zh-cn/3/library/stdtypes.html
+
+# 2.IDE 自带帮助，可以帮你补全命令
+# 通过cmd help(对象) 可以查看对象的帮助信息
+# 如在python交互式命令行中输入 help(str) 可以查看字符串的帮助信息
